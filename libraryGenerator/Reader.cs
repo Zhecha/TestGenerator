a3f7975977aa0d5ff1b0eec0cc0b7bd8e150a0eb
Zhecha <j.prejs@yandex.com>
2018-12-11 21:34:03 +0300

    add class read to stream

---
+++ libraryGenerator/Reader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace libraryGenerator
+{
+    public class Reader
+    {
+        public async Task<string> Read(string path)
+        {
+            if (path == null)
+                throw new ArgumentNullException(nameof(path));
+            StreamReader stream = new StreamReader(path);
+            return await stream.ReadToEndAsync();
+        }
+    }
+}
