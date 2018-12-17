bb3f315e2d514bc48fed204fcf84d2edb3b8ebc9
Zhecha <j.prejs@yandex.com>
2018-12-17 23:38:42 +0300

    add program console for test program

---
+++ TestsGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using libraryGenerator;
+
+namespace TestsGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reader reader = new Reader();
+            Writer writer = new Writer("Tests");
+            Generator generator = new Generator(1, 1, 1, reader, writer);
+            generator.Generate(new List<string>
+            {
+                Path.Combine("classes","Generator.cs"),
+                Path.Combine("classes","Program.cs"),
+                Path.Combine("classes","Reader.cs"),
+                Path.Combine("classes","TestClass.cs"),
+                Path.Combine("classes","Writer.cs")
+            }
+                ).Wait();
+            Console.WriteLine("Finish...");
+            Console.ReadKey();
+        }
+    }
+}
